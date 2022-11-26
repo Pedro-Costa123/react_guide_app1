4ca7d5212b218c9f60174f93b3b252850746ba56
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-11-26 17:25:25 +0000

    Section 5 : 72

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [currentYear, setFilter] = useState("All");
@@ -26,6 +27,7 @@ const Expenses = (props) => {
           selectedYear={currentYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredArray}/>
         <ExpensesList items={filteredArray} />
       </Card>
     </div>
